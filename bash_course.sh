4fc185aae231f97f59e41acf10d8d20833c145fc
dimensions22222 <alexstreamer10@gmail.com>
2026-09-24 20:52:33 +0100

    next

--- bash_course.sh
+++ bash_course.sh
@@ -439,6 +439,7 @@ echo "=========================================="
 echo "LESSON 13 — PERMISSIONS"
 echo "=========================================="
 echo
+hhhhhhhhhhhhhhhhhhhhhh
 echo "Linux files have permissions."
 echo
 echo "View permissions:"
